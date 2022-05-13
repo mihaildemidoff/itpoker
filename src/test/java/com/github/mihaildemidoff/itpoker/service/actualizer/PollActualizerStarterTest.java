222d698da3fc1bce0fe7a5515a3eed7eed14d15a
Mikhail Demidov <mihaildemidoff@gmail.com>
2022-05-13 12:40:20 +0700

    Remove native support

--- src/test/java/com/github/mihaildemidoff/itpoker/service/actualizer/PollActualizerStarterTest.java
+++ src/test/java/com/github/mihaildemidoff/itpoker/service/actualizer/PollActualizerStarterTest.java
@@ -21,10 +21,15 @@ class PollActualizerStarterTest {
     void testStart() {
         PublisherProbe<Boolean> pollActualizerProbe =
                 PublisherProbe.of(Flux.just(true));
+        PublisherProbe<Long> stuckedRequestsProbe =
+                PublisherProbe.of(Flux.just(1L));
         Mockito.when(pollActualizerService.actualizePolls())
                 .thenReturn(pollActualizerProbe.flux());
+        Mockito.when(pollActualizerService.getStuckUpdaterChain())
+                .thenReturn(stuckedRequestsProbe.flux());
         starter.onApplicationEvent(null);
         pollActualizerProbe.assertWasSubscribed();
+        stuckedRequestsProbe.assertWasSubscribed();
     }
 
 }
